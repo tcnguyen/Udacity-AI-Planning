a64b0970f2acc87ee0cc2f2e37eec560b040b08f
Canh <canh85@gmail.com>
2017-07-21 11:10:51 +0200

    finish

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -223,7 +223,12 @@ class AirCargoProblem(Problem):
         executed.
         """
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
         count = 0
+        for clause in self.goal:
+            if clause not in kb.clauses:
+                count += 1
         return count
 
 
